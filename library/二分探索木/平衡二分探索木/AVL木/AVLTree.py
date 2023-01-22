de1c06c29859aba14c9a5792bcb337bebde07ad8
akasia_midori <b1013169@gmail.com>
2023-01-22 16:09:13 +0900

    add binary search

--- library/二分探索木/平衡二分探索木/AVL木/AVLTree.py
+++ library/二分探索木/平衡二分探索木/AVL木/AVLTree.py
@@ -21,9 +21,7 @@ class AVLtree:
 
     def add(self, value, c=1):
         now = self.tree
-        if value in self.dict:
-            self.dict[value] += c
-            return 
+        if value in self.dict: self.dict[value] += c; return 
         # 値が存在しないときだけノード追加する
         else: self.dict[value] = c
         while True:
@@ -34,14 +32,11 @@ class AVLtree:
                 if now.r.h == 0: now.r = Node(value, now); self.__balance__(True, now.r); return
                 now = now.r
 
-
     def delete(self, value, c=1):
-        if value in self.dict:
-            self.dict[value] -= min(self.dict[value], c)  
+        if value in self.dict: self.dict[value] -= min(self.dict[value], c)  
         else: return False
               
-        if self.dict[value] == 0:
-            del self.dict[value]
+        if self.dict[value] == 0: del self.dict[value]
         else: return False
 
         now = self.tree.l
@@ -68,16 +63,80 @@ class AVLtree:
                     
                 return True
 
-    def find(self, value):
-        # if value in self.dict:
-        #     return True
-        # return False
+    def ge(self, value):
+        "Find the smallest element >= x, or None if it doesn't exist."
+        if self.find(value): return value
         now = self.tree.l
+        out = None
         while now.h != 0:
-            if now.value > value: now = now.l
-            elif now.value < value: now = now.r
-            else: return True
+            if value < now.value: out = now.value
+            
+            if now.l.value is None:
+                if now.r.value is None: break
+                else: now = now.r
+            elif now.r.value is None: now = now.l
+            else:
+                if value < now.l.value:  now = now.l
+                else: now = now.r
+        return out
+
+    def gt(self, value):
+        "Find the smallest element > x, or None if it doesn't exist."
+        now = self.tree.l
+        out = None
+        while now.h != 0:
+            if value < now.value: out = now.value
+            
+            if now.l.value is None:
+                if now.r.value is None: break
+                else: now = now.r
+            elif now.r.value is None: now = now.l
+            else:
+                if value < now.l.value:  now = now.l
+                else: now = now.r
+        return out
+
+    def le(self, value):
+        "Find the largest element <= x, or None if it doesn't exist."
+        if self.find(value): return value
+        now = self.tree.l
+        out = None
+        while now.h != 0:
+            if value > now.value: out = now.value
+            if now.l.value is None:
+                if now.r.value is None: break
+                else: now = now.r
+            elif now.r.value is None: now = now.l
+            else:
+                if value < now.l.value: now = now.l
+                else: now = now.r
+        return out
+
+    def lt(self, value):
+        "Find the largest element < x, or None if it doesn't exist."
+        now = self.tree.l
+        out = None
+        while now.h != 0:
+            if value > now.value: out = now.value
+            if now.l.value is None:
+                if now.r.value is None: break
+                else: now = now.r
+            elif now.r.value is None: now = now.l
+            else:
+                if value < now.l.value: now = now.l
+                else: now = now.r
+        return out
+
+    def find(self, value):
+        if value in self.dict:
+            return True
         return False
+        # now = self.tree.l
+        # while now.h != 0:
+        #     if now.value > value: now = now.l
+        #     elif now.value < value: now = now.r
+        #     else: return True
+        # return False
 
 
     def max(self):
@@ -99,19 +158,12 @@ class AVLtree:
     # 参考: http://wwwa.pikara.ne.jp/okojisan/avl-tree/iavl-tree.html
     def __replace__(self, u, v):
         p = u.p
-        if (p.l == u):
-            p.l = v
-        else:
-            p.r = v
+        if (p.l == u): p.l = v
+        else: p.r = v
         v.p = p
 
-    def __height__(self, u):
-        if u is None:
-            return 0
-        return u.h
-
     def __modHeight__(self, u):
-        u.h = 1 + max(self.__height__(u.l),self.__height__(u.r))
+        u.h = 1 + max(u.l.h, u.r.h)
 
     def __rotateR__(self, u):
         v = u.l
@@ -145,7 +197,7 @@ class AVLtree:
         return self.__rotateR__(t)
 
     def __bias__(self, u):
-        return self.__height__(u.l)-self.__height__(u.r)
+        return u.l.h - u.r.h
 
     def __balance__(self, mode, t):
         while (t.p.value != float("inf")):
@@ -165,7 +217,7 @@ class AVLtree:
                     else:
                         u = self.__rotateRL__(u)
                 else: self.__modHeight__(u)
-            if h == self.__height__(u):
+            if h == u.h:
                 break
             t = u
         
